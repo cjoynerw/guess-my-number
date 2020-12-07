d55caf919280fd3285156893f1703acc8671677b
cjoynerw <cjoynerw@gmail.com>
2020-12-07 15:54:24 -0600

    reveals number only after user guesses it correctly

--- script.js
+++ script.js
@@ -4,16 +4,25 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 // Brings in value of user guess after clicking check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'Please enter a number!';
+
+    // When the user gets the number correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'That is correct!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    // When the number guessed is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -24,6 +33,8 @@ document.querySelector('.check').addEventListener('click', function () {
         'You lost, reset and try again!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When the number guessed is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
