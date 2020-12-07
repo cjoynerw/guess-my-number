393edebce7dbff754316310cd9b62a00b2795f74
cjoynerw <cjoynerw@gmail.com>
2020-12-07 15:12:24 -0600

    added secret number generator

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// Secret number generator
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+// Brings in value of user guess after clicking check button
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please enter a number!';
+  }
 });
