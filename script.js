2ff7f49f6c90ddc7ac74b39f376ab80bea379821
cjoynerw <cjoynerw@gmail.com>
2020-12-07 17:27:19 -0600

    created reset button functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Secret secretNumber generator
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 // Brings in value of user guess after clicking check button
@@ -46,4 +46,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
 });
